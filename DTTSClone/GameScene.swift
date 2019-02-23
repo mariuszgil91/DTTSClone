92c9150c7e4c55faa65655faaf2dda9c809e66fa
Mariusz Gil <mariuszgil@macbook-air-mariusz.local>
2019-02-23 12:02:57 +0100

    touch began

--- DTTSClone/GameScene.swift
+++ DTTSClone/GameScene.swift
@@ -10,80 +10,93 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+
+    var backgroundNode: SKNode!
+    var midgroundNode: SKNode!
+    var foregroundNode: SKNode!
+    var hudNode: SKNode!
+    var startGameLabel = SKLabelNode(fontNamed: "Chalkduster")
+    let player = SKShapeNode(circleOfRadius: 20)
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     
-    override func didMove(to view: SKView) {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+    }
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+        //backgroundColor = SKColor.black
+        hudNode = SKNode()
+        addChild(hudNode)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
+        startGameLabel.text = "tap to play"
+        startGameLabel.fontSize = 30
+        startGameLabel.fontColor = SKColor.green
+        startGameLabel.position = CGPoint(x: frame.midX, y: frame.midY + 50)
+        startGameLabel.isUserInteractionEnabled = true
+        hudNode.addChild(startGameLabel)
+        
+        player.physicsBody = SKPhysicsBody(circleOfRadius: 20)
+        player.physicsBody?.isDynamic = false
+        player.physicsBody?.allowsRotation = false
+        player.position = CGPoint(x: frame.midX, y: frame.midY)
+        
+        player.physicsBody?.restitution = 1.0
+        player.physicsBody?.friction = 0.0
+        player.physicsBody?.angularDamping = 0.0
+        player.physicsBody?.linearDamping = 0.0
+        player.isUserInteractionEnabled = true
+        addChild(player)
+        
+        
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+//        let touch:UITouch = touches.first!
+//        let positionInScene = touch.location(in: self)
+//        let touchedNode = self.atPoint(positionInScene)
+//
+        // 2
+        // Remove the Tap to Start node
+        startGameLabel.removeFromParent()
+        
+        // 3
+        // Start the player by putting them into the physics simulation
+        player.physicsBody?.isDynamic = true
+        
+        // 4
+        player.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 20.0))
+        
+        print("touch")
+        
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+
+//    func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent) {
+//
+//
+//            // 2
+//            // Remove the Tap to Start node
+//            startGameLabel.removeFromParent()
+//
+//            // 3
+//            // Start the player by putting them into the physics simulation
+//            player.physicsBody?.isDynamic = true
+//
+//            // 4
+//            player.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 20.0))
+//
+//            print("touch")
+//
+//
+//
+//    }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
